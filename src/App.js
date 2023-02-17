69fb4b03f54382905a74f157ed45e0953912b168
yanshuxuan <m18888170129@163.com>
2023-02-17 15:03:28 +0800

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { createWorker } from 'tesseract.js'
+import { Component } from 'react';
+import TestOCRImage from './assests/images/testocr.png'
+import Test1Image from './assests/images/test1.png'
+// import LangPath from './tesseract-ocr-data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class index extends Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    this.demo()
+  }
+
+  demo = async () => {
+    debugger
+    const worker = await createWorker({
+      langPath: './lang-data',
+      logger: m => console.log(m),
+    })
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    // await worker.loadLanguage('chi_sim');
+    // await worker.initialize('chi_sim');
+    const { data: { text } } = await worker.recognize(Test1Image);
+    console.log(text);
+    await worker.terminate();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+
+        </header>
+      </div>
+    )
+  }
+
 }
 
-export default App;
+export default index;
